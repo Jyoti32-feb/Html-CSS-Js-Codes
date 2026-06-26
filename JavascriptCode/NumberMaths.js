70cc81fb15d3a68a8037fbab833738c20a3754d4
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-06-26 16:56:12 +0530

    Number and Math Functions And Properties

---
+++ JavascriptCode/NumberMaths.js
@@ -0,0 +1,63 @@
+let num=new Number(31452);
+// console.log(num.toString().length);
+
+// console.log(num.toString().split(""));
+
+// console.log(num.toString().split("").filter((digit)=>digit!=="-"));
+
+// console.log(num.valueOf());
+
+// console.log(num.toExponential());
+
+// console.log(num.toFixed(2));
+
+// console.log(num);
+
+let num1=100000000;
+// console.log(num1.toLocaleString("en-IN"));
+
+// console.log(num1.toLocaleString("en-US"));
+
+let num2=1234.56789;
+
+// console.log(num2.toPrecision(4));
+
+// console.log(num2.toPrecision(6));
+
+// console.log(Math.floor(8.9));
+
+// console.log(Math.ceil(8.1));
+
+// console.log(Math.round(8.5));
+
+// console.log(Math.round(8.4));
+
+// console.log(Math.max(1,2,3,4,5));
+
+// console.log(Math.min(1,2,3,4,5));
+
+console.log(Math.random());
+
+console.log(Math.random()*10);
+
+console.log(Math.floor(Math.random()*10));
+
+console.log(Math.round(Math.random()*10)+1);
+
+let min=10;
+let max=20;
+let value=Math.floor(Math.random()*(max-min+1))+min;
+console.log(value);
+
+console.log(Math.ceil(value));
+
+console.log(Math.abs(-5));
+
+console.log(Math.sqrt(25));
+
+
+
+
+
+
+
