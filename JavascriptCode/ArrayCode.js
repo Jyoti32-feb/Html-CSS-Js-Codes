465f65949bff6b56d25c17f5adaf7d79645b84aa
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-06-27 12:29:41 +0530

    practise more functions

--- JavascriptCode/ArrayCode.js
+++ JavascriptCode/ArrayCode.js
@@ -50,11 +50,31 @@ let myarr=[1,2,3,4,5,6,7,8,9,10];
 // console.log(myarr);
 
 let arr2=myarr.join();
-console.log(arr2);
+// console.log(arr2);
 
-console.log(typeof(arr2));
+// console.log(typeof(arr2));
 
-console.log(myarr.includes(10));
-console.log(myarr.indexOf(7));
-console.log(myarr.concat(11,12,13,14,15));
-console.log(myarr);
+// console.log(myarr.includes(10));
+// console.log(myarr.indexOf(7));
+// console.log(myarr.concat(11,12,13,14,15));
+// console.log(myarr);
+
+
+let arr3=["jyoti","pathade",[1,,23,32,4],[3,4,5]];
+console.log(arr3[2][2]);
+
+let arr4=arr3.flat(Infinity);
+console.log(arr4);
+
+console.log(Array.isArray("jyoti"));
+console.log(Array.from("jyoti"));
+console.log(Array.from({name:"jyoti"}));//
+
+console.log(Array.of(1,2,3,"school","ice",{name :"jyoti",email:"abc@gmail.com"}));
+
+let arr5=[12,21,23,32,22,33];
+let arr6=[11,12,21,24,25,35,45,54,53];
+let newarr=[...arr5,...arr6];
+console.log(newarr);
+
+
