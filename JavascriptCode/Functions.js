f016b02976746db1dfa299bb2edd95722590dfe7
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-06-27 15:40:20 +0530

    functions and scoping with hoisting

---
+++ JavascriptCode/Functions.js
@@ -0,0 +1,65 @@
+function check(username , validation){
+    if(!validation){
+        console.log("enter valid value :")
+        return;
+    }
+    console.log(`hey , ${username} is logged in : ${validation}`);
+};
+
+
+check( "jyoti" ,);
+
+const cal=function(num1,num2){
+    return num1+num2;
+};
+console.log(cal(2,3));
+
+function calculator(...num){
+    return num;
+}
+console.log(calculator(1100,1200,13000));
+
+const obj={
+    name :"jyoti",
+    email : "abc@gmail.com"
+};
+
+function print(getanyobj){
+    return getanyobj;
+};
+//console.log(print(obj));
+console.log(print({name :"yogesh",lastname:"Pathade"}));
+
+
+function printArray(getArray){
+    return getArray;
+}
+const arr=[{name :"mauli ",lastname :"Pathade "},100, 70];
+//console.log(printArray(arr));
+
+console.log(printArray([1,2,3,4,5]));
+
+function one(){
+    const username ="jyoti";
+    function two(){
+        const website=" google";
+        console.log(username + website);
+
+    }
+    console.log(website);//scoping problem 
+    two();
+}
+one();
+
+greet();
+function greet (){
+    console.log("hey ...welcome to google !");
+}
+
+//greettwo();// not allowed in case of function expression
+const greettwo=function(){
+    console.log("hey .. how are you");
+}
+greettwo()
+
+
